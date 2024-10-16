44f023ff5c6ba3d46e24561acff9feffff3b5123
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-10-16 16:17:36 +0200

    ol

--- javaScript.js
+++ javaScript.js
@@ -38,3 +38,12 @@ document.getElementById('nextButton').addEventListener('click', function() {
 // Initially show the first page
 document.getElementById('page1').style.display = 'flex';
 document.getElementById('page2').style.display = 'none';
+
+
+$(document).ready(function() {
+  $('#nextButton').on('click', function() {
+      // Logic to go to the next page
+      // For example, you could navigate to another comic page
+      window.location.href = 'nextComic.html'; // Change this to your desired page
+  });
+});
